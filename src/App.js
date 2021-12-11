83a8a643c3931c031544e19d0292be22abe4c086
Simon Lee <simonhlee97@hotmail.com>
2021-12-11 12:42:06 +0900

    finished section 4 forms, validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData)
+
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
 
 	const deleteFeedback = (id) => {
 		// console.log('App', id)
@@ -17,6 +25,8 @@ function App() {
 		<>
 			<Header />
 			<div className="container">
+				<FeedbackForm handleAdd={addFeedback} />
+				<FeedbackStats feedback={feedback} />
 				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
 		</>
